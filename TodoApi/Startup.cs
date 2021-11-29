0847caf4c3d54e1f5d14967d132bb3783cbbb3a5
Luke Patterson <lukepatterson31@gmail.com>
2021-11-29 16:26:48 +0000

    serves swagger ui at app's root

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -70,6 +70,7 @@ namespace TodoApi
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+                c.RoutePrefix = string.Empty;
             });
 
 
