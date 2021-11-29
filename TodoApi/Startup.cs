6cb674d9069ba90a0a2d17d9f95e78b6fdafca1b
Luke Patterson <lukepatterson31@gmail.com>
2021-11-29 15:14:27 +0000

    removed swagger

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -23,9 +23,6 @@ namespace TodoApi
             services.AddControllers();
             services.AddDbContext<TodoContext>(opt =>
                 opt.UseInMemoryDatabase("TodoList"));
-            
-            // Register the Swagger generator, defining 1 or more Swagger documents.
-            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,13 +36,6 @@ namespace TodoApi
             app.UseDefaultFiles();
             app.UseStaticFiles();
             
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
-            app.UseSwaggerUI();
-
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
